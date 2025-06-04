9f2ce9512829e3db5ea25d9aedc62d9d9aa70a60
pedrofrohmut <pedrofrohmut@gmail.com>
2025-06-04 15:48:35 -0300

    Close Tickets

--- rodrigo-branas/persistence-patterns/src/main.ts
+++ rodrigo-branas/persistence-patterns/src/main.ts
@@ -7,39 +7,79 @@ const app = express()
 app.use(express.json())
 
 app.post("/tickets", async (req, res) => {
-    const ticketId = crypto.randomUUID()
     const connection = pgp()("postgres://postgres:password@localhost:5108")
+
+    const ticketId = crypto.randomUUID()
     await connection.query(
         "insert into tickets (ticket_id, requester_id, content, start_date, status) values ($1, $2, $3, $4, $5)",
         [ticketId, req.body.requesterId, req.body.content, new Date(), "open"]
     )
+
     await connection.$pool.end()
+
     res.json({ ticketId })
 })
 
 app.get("/tickets/:ticketId", async (req, res) => {
     const connection = pgp()("postgres://postgres:password@localhost:5108")
+
     const [ticketData] = await connection.query("select * from tickets where ticket_id = $1", [
         req.params.ticketId
     ])
+
     await connection.$pool.end()
+
     res.json({
         ticketId: ticketData.ticket_id,
         requesterId: ticketData.requester_id,
         assigneeId: ticketData.assignee_id,
         startDate: ticketData.start_date,
         status: ticketData.status,
-        content: ticketData.content
+        content: ticketData.content,
+        endDate: ticketData.end_date,
+        duration: ticketData.duration
     })
 })
 
-app.put("/tickets/:ticketId", async (req, res) => {
+app.post("/tickets/:ticketId/assign", async (req, res) => {
     const connection = pgp()("postgres://postgres:password@localhost:5108")
+
+    const [ticket] = await connection.query("select * from tickets where ticket_id = $1", [
+        req.params.ticketId
+    ])
+    if (ticket.status === "assigned") {
+        throw new Error("This ticket cannot be assigned, because it is already closed.")
+    }
+
     await connection.query(
         "update tickets set status = $1, assignee_id = $2 where ticket_id = $3",
         [req.body.status, req.body.assigneeId, req.params.ticketId]
     )
+
     await connection.$pool.end()
+
+    res.end()
+})
+
+app.post("/tickets/:ticketId/close", async (req, res) => {
+    const connection = pgp()("postgres://postgres:password@localhost:5108")
+
+    const [ticket] = await connection.query("select * from tickets where ticket_id = $1", [
+        req.params.ticketId
+    ])
+    if (ticket.status === "open") {
+        throw new Error("This ticket is open. Cannot close a ticket that was not assigned.")
+    }
+
+    const endDate = new Date()
+    const duration = endDate.getTime() - ticket.start_date.getTime()
+    await connection.query(
+        "update tickets set status = $1, duration = $2, end_date = $3  where ticket_id = $4",
+        [req.body.status, duration, endDate, req.params.ticketId]
+    )
+
+    await connection.$pool.end()
+
     res.end()
 })
 
--- rodrigo-branas/persistence-patterns/tests/api.test.ts
+++ rodrigo-branas/persistence-patterns/tests/api.test.ts
@@ -1,44 +1,73 @@
 import axios from "axios"
 
-const BASE_URL = "http://localhost:5000/tickets"
+const BASE_URL = "http://localhost:5000"
 
 test("Should create a new ticket", async () => {
     // Request Data
-    const input1 = {
+    const input = {
         requesterId: "ebd9aa7c-2be2-4439-ad9a-2c16ea568bda",
         content: "The internet is too slow"
     }
 
     // Create new ticket
-    const response = await axios.post(BASE_URL, input1)
-    const output = response.data
+    const { data: output } = await axios.post(`${BASE_URL}/tickets`, input)
 
     // Read the ticket newly created
-    const response2 = await axios.get(`${BASE_URL}/${output.ticketId}`)
-    const output2 = response2.data
+    const { data: output2 } = await axios.get(`${BASE_URL}/tickets/${output.ticketId}`)
 
     // Validate response ticket
     expect(output2.ticketId).toBeDefined()
-    expect(output2.requesterId).toBe(input1.requesterId)
-    expect(output2.content).toBe(input1.content)
+    expect(output2.requesterId).toBe(input.requesterId)
+    expect(output2.content).toBe(input.content)
     expect(output2.status).toBe("open")
 })
 
 test("Should assign a ticket to an attendant", async () => {
+    // Create new ticket
     const input1 = {
         requesterId: "ebd9aa7c-2be2-4439-ad9a-2c16ea568bda",
         content: "The internet is too slow"
     }
-    const { data: output1 } = await axios.post(BASE_URL, input1)
+    const { data: output1 } = await axios.post(`${BASE_URL}/tickets`, input1)
 
+    // Assign to the ticket an attendant
     const input2 = {
         assigneeId: "840580b4-3653-4fcd-87cb-12c46d1aabd4",
-        status: "assignned"
+        status: "assigned"
     }
-    await axios.put(`${BASE_URL}/${output1.ticketId}`, input2)
+    await axios.post(`${BASE_URL}/tickets/${output1.ticketId}/assign`, input2)
 
-    const { data: output3 } = await axios.get(`${BASE_URL}/${output1.ticketId}`)
-
+    // Check if the ticket created has the assigned attendant
+    const { data: output3 } = await axios.get(`${BASE_URL}/tickets/${output1.ticketId}`)
     expect(output3.assigneeId).toBe(input2.assigneeId)
     expect(output3.status).toBe(input2.status)
+})
+
+test("Should close a ticket", async () => {
+    // Create new ticket
+    const input1 = {
+        requesterId: "ebd9aa7c-2be2-4439-ad9a-2c16ea568bda",
+        content: "The internet is too slow"
+    }
+    const { data: output1 } = await axios.post(`${BASE_URL}/tickets`, input1)
+
+    // Assign to the ticket an attendant
+    const input2 = {
+        assigneeId: "840580b4-3653-4fcd-87cb-12c46d1aabd4",
+        status: "assigned"
+    }
+    await axios.post(`${BASE_URL}/tickets/${output1.ticketId}/assign`, input2)
+
+    // Close the ticket
+    const input3 = {
+        status: "closed"
+    }
+    await axios.post(`${BASE_URL}/tickets/${output1.ticketId}/close`, input3)
+
+    // Check if the ticket created has the assigned attendant and status 'closed'
+    const { data: output2 } = await axios.get(`${BASE_URL}/tickets/${output1.ticketId}`)
+    expect(output2.assigneeId).toBe(input2.assigneeId)
+    expect(output2.endDate).toBeDefined()
+    expect(output2.duration).toBeDefined()
+    expect(output2.status).toBe(input3.status)
 })
